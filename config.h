bfa556f40846065f023ea4db10672ebc9f6b9f37
ashelly <adam@keymeonline.com>
2014-07-13 23:10:38 -0400

    initial FM defaults

--- config.h
+++ config.h
@@ -30,7 +30,7 @@
 
 // Default settings. Used when resetting EEPROM. Change to desired name in defaults.h
 //#define DEFAULTS_GENERIC
-#define DEFAULTS_EXCITRON
+#define DEFAULTS_KEYME_FM
 
 // Serial baud rate
 #define BAUD_RATE 57600
