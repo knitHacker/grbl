f2b2bc9c1cce28289fa27693f5536308d346968e
Adit Namdev <adit.namdev@key.me>
2016-06-01 12:13:03 -0400

    updated description of MAXFLAGLEN

--- limits.h
+++ limits.h
@@ -22,7 +22,9 @@
 #ifndef limits_h
 #define limits_h 
 
-// Size of the flag in mm
+// Largest possible distance to move during pulloff motion of homing.
+// This is defined by the largest homing flag in the system. The Y flag is
+// the largest with a length of just under 40 mm.
 #define MAXFLAGLEN 40.0
 
 typedef struct {
